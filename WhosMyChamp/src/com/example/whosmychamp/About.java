d5a48f1c337dfe1082a0137abd716e0576035149
mscho88 <minsu.cho@mail.utoronto.ca>
2014-09-14 02:24:49 -0400

    Almost done

--- WhosMyChamp/src/com/example/whosmychamp/About.java
+++ WhosMyChamp/src/com/example/whosmychamp/About.java
@@ -1,12 +1,34 @@
 package com.example.whosmychamp;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class About extends Activity{
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 	    setContentView(R.layout.about);
+	    
+	    Typeface typeFace = Typeface.createFromAsset(getAssets(),"fonts/lolfont.ttf");
+	    TextView txt = (TextView) findViewById(R.id.aboutTxt);
+	    txt.setTypeface(typeFace);
+	    txt = (TextView) findViewById(R.id.textView1);
+	    txt.setTypeface(typeFace);
+	    
+	    Button closebtn = (Button) findViewById(R.id.button1);
+	    closebtn.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View v){
+		        About.this.finish();
+
+				//startActivity(new Intent(About.this, MainActivity.class));
+			}
+		});
 	}
 }
